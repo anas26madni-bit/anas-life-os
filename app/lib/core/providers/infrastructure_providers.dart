76ab53462f821a254fe735cae3c6bc301fe37e64
anas26madni-bit <anas26madni@gmail.com>
2026-07-30 18:07:03 +0500

    Wire Sprint 3 repositories

--- app/lib/core/providers/infrastructure_providers.dart
+++ app/lib/core/providers/infrastructure_providers.dart
@@ -2,9 +2,13 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../features/database_foundation/data/database/app_database.dart';
 import '../../features/tasks/data/repositories/drift_project_repository.dart';
+import '../../features/tasks/data/repositories/drift_task_composition_repository.dart';
 import '../../features/tasks/data/repositories/drift_task_repository.dart';
+import '../../features/tasks/data/repositories/drift_task_support_repository.dart';
 import '../../features/tasks/domain/repositories/project_repository.dart';
+import '../../features/tasks/domain/repositories/task_composition_repository.dart';
 import '../../features/tasks/domain/repositories/task_repository.dart';
+import '../../features/tasks/domain/repositories/task_support_repository.dart';
 import '../database/android_database_platform.dart';
 import '../database/database_file_resolver.dart';
 import '../database/database_initializer.dart';
@@ -37,6 +41,18 @@ final taskRepositoryProvider = FutureProvider<TaskRepository>((ref) async {
   final database = await ref.watch(appDatabaseProvider.future);
   return DriftTaskRepository(database);
 });
+
+final taskSupportRepositoryProvider = FutureProvider<TaskSupportRepository>((
+  ref,
+) async {
+  final database = await ref.watch(appDatabaseProvider.future);
+  return DriftTaskSupportRepository(database);
+});
+final taskCompositionRepositoryProvider =
+    FutureProvider<TaskCompositionRepository>((ref) async {
+      final database = await ref.watch(appDatabaseProvider.future);
+      return DriftTaskCompositionRepository(database);
+    });
 
 final projectRepositoryProvider = FutureProvider<ProjectRepository>((
   ref,
