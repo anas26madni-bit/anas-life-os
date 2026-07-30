ff19eed32c510f4f95dc981ce6b9d8a494b18af1
anas26madni-bit <anas26madni@gmail.com>
2026-07-30 12:21:15 +0500

    Apply Sprint 2 database foundation

---
+++ app/test/helpers/database_test_harness.dart
@@ -0,0 +1,12 @@
+import 'dart:typed_data';
+
+import 'package:anas_life_os/core/database/database_key.dart';
+import 'package:anas_life_os/features/database_foundation/data/database/app_database.dart';
+
+DatabaseKey testDatabaseKey([int seed = 1]) => DatabaseKey(
+  Uint8List.fromList(
+    List<int>.generate(32, (index) => (index + seed) % 256),
+  ),
+);
+
+AppDatabase createTestDatabase() => AppDatabase.inMemory(testDatabaseKey());
