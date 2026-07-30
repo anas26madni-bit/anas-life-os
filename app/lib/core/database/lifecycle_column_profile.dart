84928107e89f8cb6be3d50f1c1a7a4e67ae9937d
anas26madni-bit <anas26madni@gmail.com>
2026-07-30 12:11:26 +0500

    Apply Sprint 2 database foundation

---
+++ app/lib/core/database/lifecycle_column_profile.dart
@@ -0,0 +1,26 @@
+abstract final class LifecycleColumnProfile {
+  static const businessEntity = <String>{
+    'id',
+    'uuid',
+    'created_at',
+    'updated_at',
+    'deleted_at',
+    'is_deleted',
+    'sync_status',
+    'version',
+    'created_by',
+    'updated_by',
+    'notes',
+  };
+
+  static const history = <String>{
+    'id',
+    'uuid',
+    'created_at',
+  };
+
+  static const schema = <String>{
+    'id',
+    'uuid',
+  };
+}
